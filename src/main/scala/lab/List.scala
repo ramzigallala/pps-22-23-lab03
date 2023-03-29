c4b3a5d5f75b219329e050649e346c36ef4cf8e0
Ramzi Gallala <ramzi.gallala@studio.unibo.it>
2023-03-29 22:11:00 +0200

    part2 completed

--- src/main/scala/lab/List.scala
+++ src/main/scala/lab/List.scala
@@ -1,5 +1,8 @@
 package lab
 
+import u02.Modules.Person
+import u02.Modules.Person.Teacher
+
 enum List[E]:
   case Cons(head: E, tail: List[E])
   case Nil()
@@ -23,6 +26,34 @@ object List:
     case Cons(head, tail) if tail!=Nil() => if(head>max(tail).get) Some(head) else max(tail)
     case Cons(head, tail) if tail==Nil() => Some(head)
     case Nil()=> None
+
+  def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
+  case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
+  case Nil() => Nil()
+
+  def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
+  case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
+  case Cons(_, t) => filter (t)(pred)
+  case Nil() => Nil()
+
+
+  def CoursesTeacher[A](l: List[A]): List[A] =
+    var listT= filter(l)(_.isInstanceOf[Person.Teacher])
+    return map(listT)(_.asInstanceOf[Person.Teacher].course.asInstanceOf[A])
+
+
+  def foldLeft(l: List[Int])(defValue: Int)(f: (x:Int, y:Int) => Int): Int = l match
+    case Cons(head, tail) if tail!=Nil() => f(foldLeft(tail)(defValue)(f), head)
+    case Cons(head, tail) if tail==Nil() => f(defValue, head)
+    case Nil() => defValue
+
+  def foldRight(l: List[Int])(defValue: Int)(f: (Int, Int) => Int): Int = l match
+    case Cons(head, tail) if tail != Nil()=> f(head, foldRight(tail)(defValue)(f))
+    case Cons(head, tail) if tail == Nil() => f(head, defValue)
+    case Nil() => defValue
+
+
+
 
 
 
--- src/test/scala/lab/ListPartOne.scala
+++ src/test/scala/lab/ListPartOne.scala
@@ -3,9 +3,11 @@ package lab
 import org.junit.Assert.*
 import org.junit.Test
 import List.*
+import u02.Modules.Person
 
 class ListPartOne:
   val lst = Cons (10 , Cons (20 , Cons (30 , Nil () )) )
+  val lstFold = Cons (3 , Cons (7 , Cons (1 , Cons(5, Nil()) )) )
   @Test def testDrop() =
     assertEquals(drop(lst,1),Cons (20 , Cons(30, Nil())))
     assertEquals(drop(lst, 2), Cons(30, Nil()))
@@ -22,9 +24,19 @@ class ListPartOne:
     val momT = Cons (11 , Cons (12 , Cons (21 , Cons (22 , Cons (31 , Cons (32 , Nil ()))))))
     assertEquals(mom, momT)
 
-  @Test def testMap =
+  @Test def testMax =
     assertEquals(max(Cons(10, Cons(25, Cons(20, Nil())))), Some(25))
     assertEquals(max(Nil()), None)
+
+  @Test def testCourses() =
+    val listPerson = Cons(Person.Teacher(" mario ", "fisica"), Cons(Person.Student(" mari ", 2015), Nil()))
+    println(CoursesTeacher(listPerson))
+
+  @Test def testFoldLeft() =
+    assertEquals(foldLeft(lstFold)(0)(_-_), -16)
+
+  @Test def testFoldRight() =
+    assertEquals(foldRight(lstFold)(0)(_-_), -8)
 
 
 
