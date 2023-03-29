a41b37c4c3dc3c4e4dcd78f0eb60d7431309e777
Ramzi Gallala <ramzi.gallala@studio.unibo.it>
2023-03-29 13:52:59 +0200

    part1 completed

--- src/main/scala/lab/List.scala
+++ src/main/scala/lab/List.scala
@@ -10,3 +10,20 @@ object List:
     case Cons(head, tail) if n> 0 => drop(tail, n-1)
     case Cons(head, tail) if n==0 => Cons(head, tail)
     case Nil() => Nil()
+
+  def append[A](left: List[A], right: List[A]): List[A] = left match
+    case Cons(head, tail) => Cons(head, append(tail, right))
+    case Nil() => right
+
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = l match
+    case Cons(head, tail) => append(f(head),flatMap(tail)(f))
+    case Nil()=> Nil()
+
+  def max(l: List[Int]): Option[Int] = l match
+    case Cons(head, tail) if tail!=Nil() => if(head>max(tail).get) Some(head) else max(tail)
+    case Cons(head, tail) if tail==Nil() => Some(head)
+    case Nil()=> None
+
+
+
+
--- src/test/scala/lab/ListPartOne.scala
+++ src/test/scala/lab/ListPartOne.scala
@@ -10,3 +10,23 @@ class ListPartOne:
     assertEquals(drop(lst,1),Cons (20 , Cons(30, Nil())))
     assertEquals(drop(lst, 2), Cons(30, Nil()))
     assertEquals(drop(lst, 5), Nil())
+
+  @Test def testAppend() =
+    val tail = Cons(40, Nil())
+    assertEquals(append(lst, tail),Cons (10 , Cons (20 , Cons (30 , Cons (40 , Nil ())))))
+
+  @Test def testFlatMap() =
+    var mom = flatMap(lst)(v => Cons ( v + 1, Nil()))
+    assertEquals(mom, Cons (11 , Cons(21 , Cons(31 , Nil()))))
+    mom = flatMap ( lst )(v => Cons ( v + 1, Cons (v + 2, Nil () )))
+    val momT = Cons (11 , Cons (12 , Cons (21 , Cons (22 , Cons (31 , Cons (32 , Nil ()))))))
+    assertEquals(mom, momT)
+
+  @Test def testMap =
+    assertEquals(max(Cons(10, Cons(25, Cons(20, Nil())))), Some(25))
+    assertEquals(max(Nil()), None)
+
+
+
+
+
